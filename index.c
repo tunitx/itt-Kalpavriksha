07170b0028a44e9b397066a0ae2dcd5b32b8b7ca
tunitx <imta819@gmail.com>
2025-01-10 21:53:22 +0530

    q-6

--- index.c
+++ index.c
@@ -1,58 +1,35 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 #define max_size 101
-
-int is_digit(char c){
-return (c >= '0' && c <= '9');
-}
-
 int main() {
     // Write C code here
-    char arr[max_size];
-    printf("enter the string: ");
-    fgets(arr, max_size, stdin);
+    int str1[26] = {0};
+    int str2[26] = {0};
+    char s1[max_size];
+    char s2[max_size];
     
-    int end = 0;
-    for(; arr[end]!='\n'; end ++){
-        
+    printf("enter the s1 : ");
+    scanf("%s", s1);
+    while(getchar()!='\n'){}
+    
+    printf("enter the s2 : ", s2);
+    scanf("%s", s2);
+    
+    for(int i =0; s1[i]!='\0'; i++){
+        str1[s1[i] - 'a'] ++;
     }
-    
-    
-    arr[end] = '\0';
-    end --;
-   while(end >=0 && arr[end] == ' '){
-       end --;
-   }
-   int start =0;
-   while(start <= end && arr[start] == ' '){
-       start ++;
-   }
-   if(start > end){
-       printf("invalid input");
-       return 0;
-   }
-   int ans = 0, invalid = 0, neg = 1;
-   if(arr[start] == '-'){
-        neg = -1;
-        start ++;
-   }
-   else if(arr[start] == '+') start ++;
-   while(start <= end){
-       if(!is_digit(arr[start])){
-           invalid = 1;
-           break;
-       }
-       else{
-           ans = ans*10 + (arr[start] - '0');
-           start ++;
-       }
-   }
-  if (invalid) {
-        printf("Invalid input");
-    } else {
-        printf("The number is: %d", ans * neg);
+    for(int  i= 0; s2[i]!='\0'; i++){
+        str2[s2[i] - 'a'] ++;
     }
-    
+    int invalid = 0;
+    for(int i =0; i<26; i++){
+        if(str1[i]!=str2[i]){
+            invalid = 1;
+            break;
+        }
+    }
+    if(invalid)printf("they arent anagram");
+    else printf("yes they are anagram");
 
     return 0;
 }
