ebe9186bdeca4759c3ddbc9a06832d788eb1a797
tunitx <imta819@gmail.com>
2025-01-10 21:43:02 +0530

    q-3

--- index.c
+++ index.c
@@ -1,34 +1,38 @@
 // Online C compiler to run C program online
 #include <stdio.h>
+#include <string.h>
 #define max_size 101
 
+int helper(char *arr,int i, char *sub){
+    int j =0;
+    while(arr[i]!= '\0' && sub[j]!='\0'){
+        if(arr[i]!=sub[j]) return 0;
+        i++;
+        j++;
+    }
+    return (sub[j] == '\0');
+    
+}
 
 int main() {
    char arr[max_size];
-   printf("enter the string: ");
+   char sub[max_size];
+   printf("enter the string : ");
    fgets(arr, max_size, stdin);
-//   while(getchar()!= '\n');
+   arr[strcspn(arr, "\n")] = '\0';
    
-   int end = 0;
-   for(int i =0; i<max_size; i++){
-       if(arr[i] == '\n'){
-           arr[i] = '\0';
-           end = i;
+   printf("enter the substring: ");
+   scanf("%s", sub);
+   
+   int ans =0, not_found = 0;
+   for(int i =0; arr[i]!='\0'; i++){
+       if(helper(arr, i , sub)){
+           not_found = 1;
+           ans = i;
            break;
        }
    }
-   end -- ;
-while(arr[end] ==' ' )end --;
-   int count  =0, i =0;
-   while(arr[i] == ' ')i++;
-   
-   while(i<=end){
-     if (arr[i] != ' ' && (i == end || arr[i + 1] == ' ')) {
-            count++;
-        }
-        i++;
-   }
-   printf("the number of words is: %d", count);
-
+   if(not_found == 0) printf("not found");
+   else  printf("the ans is : %d", ans);
     return 0;
 }
