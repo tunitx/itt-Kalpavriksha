4b883787bba1d89b25c6123c9a9a6bb3aea60952
tunitx <imta819@gmail.com>
2025-01-10 22:39:29 +0530

    q-7

--- index.c
+++ index.c
@@ -1,35 +1,54 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 #define max_size 101
+
 int main() {
-    // Write C code here
-    int str1[26] = {0};
-    int str2[26] = {0};
-    char s1[max_size];
-    char s2[max_size];
-    
-    printf("enter the s1 : ");
-    scanf("%s", s1);
-    while(getchar()!='\n'){}
-    
-    printf("enter the s2 : ", s2);
-    scanf("%s", s2);
-    
-    for(int i =0; s1[i]!='\0'; i++){
-        str1[s1[i] - 'a'] ++;
+    char arr[max_size];
+    printf("Enter the sentence: ");
+    fgets(arr, max_size, stdin);
+    int end = 0;
+    while (arr[end] != '\n' && arr[end] != '\0') {
+        end++;
     }
-    for(int  i= 0; s2[i]!='\0'; i++){
-        str2[s2[i] - 'a'] ++;
+    arr[end--] = '\0';
+    while (end >= 0 && arr[end] == ' ') {
+        end--;
     }
-    int invalid = 0;
-    for(int i =0; i<26; i++){
-        if(str1[i]!=str2[i]){
-            invalid = 1;
-            break;
+    int start = 0;
+    while (start <= end && arr[start] == ' ') {
+        start++;
+    }
+
+    if (start > end) {
+        printf("No words found.\n");
+        return 0;
+    }
+
+    int max_len = 0, curr_len = 0;
+    int start_index = start, max_start = start;
+
+    for (int i = start; i <= end; i++) {
+        if (arr[i] != ' ') {
+            curr_len++;
+        } else {
+            if (curr_len > max_len) {
+                max_len = curr_len;
+                max_start = i - curr_len;
+            }
+            curr_len = 0;
         }
     }
-    if(invalid)printf("they arent anagram");
-    else printf("yes they are anagram");
 
+    
+    if (curr_len > max_len) {
+        max_len = curr_len;
+        max_start = end - curr_len + 1;
+    }
+    char ans[max_len + 1];
+    for (int i = 0; i < max_len; i++) {
+        ans[i] = arr[max_start + i];
+    }
+    ans[max_len] = '\0';
+
+    printf("Longest word is: %s\n", ans);
     return 0;
 }
