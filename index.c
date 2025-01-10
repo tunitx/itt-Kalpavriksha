eb8a7b5845b9286d443fcb434b7f375a631fd340
tunitx <imta819@gmail.com>
2025-01-10 21:44:39 +0530

    q-5

--- index.c
+++ index.c
@@ -1,57 +1,58 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 #define max_size 101
-int helper(char *temp, int i, char * s1){
-    int j =0;
-    while(temp[i]!= '\0' && s1[j]!= '\0'){
-        if(temp[i]!=s1[j]) return 0;
-        j++;
-        i++;
-    }
-    return (s1[j] == '\0');
+
+int is_digit(char c){
+return (c >= '0' && c <= '9');
 }
+
 int main() {
     // Write C code here
-    char s1[max_size];
-    char s2[max_size];
+    char arr[max_size];
+    printf("enter the string: ");
+    fgets(arr, max_size, stdin);
     
-    printf("enter the s1: ");
-    scanf("%s", s1);
-    
-    getchar();
-    
-    printf("enter the s2: ");
-    scanf("%s", s2);
-    getchar();
-    
-    int len1 =0, len2 =0;
-    for(int i =0; s1[i]!='\0'; i++){
-        len1 ++;
-    }
-    for(int i =0; s2[i]!='\0'; i++){
-        len2 ++;
+    int end = 0;
+    for(; arr[end]!='\n'; end ++){
+        
     }
     
-    if(len1!=len2){
-        printf("false");
-        return 0;
-    }
-    char temp [len1 + len2 + 1];
-    int j =0;
-    for(int i =0; s2[i]!='\0'; i++){
-        temp[j++] = s2[i]; 
-    }
-    for(int i =0; s2[i]!='\0'; i++){
-        temp[j++] = s2[i];
-    }
-    temp[j] = '\0';
     
-    for(int i =0; temp[i]!='\0'; i++){
-        if(helper(temp, i, s1)){
-            printf("true");
-            return 0;
-        } 
+    arr[end] = '\0';
+    end --;
+   while(end >=0 && arr[end] == ' '){
+       end --;
+   }
+   int start =0;
+   while(start <= end && arr[start] == ' '){
+       start ++;
+   }
+   if(start > end){
+       printf("invalid input");
+       return 0;
+   }
+   int ans = 0, invalid = 0, neg = 1;
+   if(arr[start] == '-'){
+        neg = -1;
+        start ++;
+   }
+   else if(arr[start] == '+') start ++;
+   while(start <= end){
+       if(!is_digit(arr[start])){
+           invalid = 1;
+           break;
+       }
+       else{
+           ans = ans*10 + (arr[start] - '0');
+           start ++;
+       }
+   }
+  if (invalid) {
+        printf("Invalid input");
+    } else {
+        printf("The number is: %d", ans * neg);
     }
-    printf("false");
+    
+
     return 0;
 }
