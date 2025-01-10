9dca7417bce95b2a60699edc4b21d37afaafb3c1
tunitx <imta819@gmail.com>
2025-01-10 21:41:30 +0530

    q-2

--- index.c
+++ index.c
@@ -1,27 +1,34 @@
-#include<stdio.h>
+// Online C compiler to run C program online
+#include <stdio.h>
+#define max_size 101
 
-int main(){
-    char arr[100];
-    printf("enter the string: ");
-    scanf("%s", arr);
-    
-    int chars[26] = {0};
-    char op[100];
-    
-    int i =0, j =0;
-    while(arr[i]!='\0'){
-        if(chars[(int)(arr[i] - 'a')]){
-            i++;
-            continue;
+
+int main() {
+   char arr[max_size];
+   printf("enter the string: ");
+   fgets(arr, max_size, stdin);
+//   while(getchar()!= '\n');
+   
+   int end = 0;
+   for(int i =0; i<max_size; i++){
+       if(arr[i] == '\n'){
+           arr[i] = '\0';
+           end = i;
+           break;
+       }
+   }
+   end -- ;
+while(arr[end] ==' ' )end --;
+   int count  =0, i =0;
+   while(arr[i] == ' ')i++;
+   
+   while(i<=end){
+     if (arr[i] != ' ' && (i == end || arr[i + 1] == ' ')) {
+            count++;
         }
-        else{
-            chars[(int)(char)(arr[i] - 'a')] = 1;
-            op[j] = arr[i];
-            i++;
-            j++;
-        }
-    }
-    op[j] = '\0';
-    
-    printf("ther op is : %s", op);
+        i++;
+   }
+   printf("the number of words is: %d", count);
+
+    return 0;
 }
