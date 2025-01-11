e680c6c5003d92ccbad90b64dc0a69772e19fe6d
tunitx <imta819@gmail.com>
2025-01-11 21:34:02 +0530

    permutation

--- index.c
+++ index.c
@@ -1,47 +1,56 @@
-#include<stdio.h>
-#define max_size 101
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int is_digit(char c){
-    return c >= '0' && c<='9';
+#define max_size 1000
+int factorial(int n) {
+    if (n <= 1) return 1;
+    return n * factorial(n - 1);
 }
-
-int helper(char * temp, int size){
-    int i =0, num =0;
-    if(temp[i] == '0' && size>1)return 0;
-    while(temp[i]!='\0'){
-        if(!is_digit(temp[i]))return 0;
-        num = num*10 + temp[i] - '0';
-        if(num > 255) return 0;
-        i++;
+void helper(char * str, int * chars, char * curr_str, char ** ans, int size, int  curr_str_index, int * ans_index){
+    if(curr_str_index == size ){
+        curr_str[curr_str_index] = '\0';
+       strcpy(ans[*ans_index], curr_str);
+        (*ans_index)++ ;
+        return;
     }
-    return num <=255;
+    
+    for(int i =0; str[i]!='\0'; i++){
+        if(chars[str[i] - 'a'] !=0)continue;
+        
+        curr_str[curr_str_index] = str[i];
+        chars[str[i] - 'a'] = 1;
+        helper(str, chars, curr_str, ans, size, curr_str_index+1, ans_index);
+        chars[str[i] - 'a'] = 0;
+    }
 }
 
-int main(){
-    char arr[max_size];
-    printf("enter the IP: ");
-    scanf("%s", arr);
+int main() {
+    // Write C code here
+    int chars[26]= {0};
+    char str[max_size];
+    printf("enter the string : ");
+    scanf("%s", str);
     getchar();
-    
-    int i =0, invalid = 0, segment = 0;
-    while(arr[i]!='\0'){
-        char temp[max_size];
-        int k =0;
-        while(arr[i]!='.' && arr[i]!='\0'){
-            temp[k++] = arr[i];
-            i++;
-        }
-        temp[k] = '\0';
-        int res = helper(temp, k);
-        if(!res){
-            invalid = 1;
-            break;
-        }
-        segment ++;
-        i++;
+    int size = 0;
+    for(; str[size]!='\0'; size ++){
     }
-    if(invalid || segment !=4)printf("invalid ip");
-    else printf("valid ip");
+    int permutations = factorial(size);
     
+    char **ans = (char **)malloc(permutations * sizeof(char *));
+    for(int i =0; i<permutations; i++){
+        ans[i] = (char* )malloc((size + 1) * sizeof(char));
+    }
+    
+    char curr_str[max_size];
+    int curr_str_index = 0, ans_index = 0;
+    helper(str, chars, curr_str, ans, size, curr_str_index, &ans_index);
+     printf("Permutations:\n");
+    for (int i = 0; i < ans_index; i++) {
+        printf("%s\n", ans[i]);
+        free(ans[i]);  
+    }
+    free(ans);
     return 0;
-}
+}ad
