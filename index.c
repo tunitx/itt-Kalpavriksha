c781e4013deeef60f3125f1900f410a764a7b73d
tunitx <imta819@gmail.com>
2025-01-11 12:44:53 +0530

    q-9

--- index.c
+++ index.c
@@ -1,54 +1,41 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 #define max_size 101
 
 int main() {
+    // Write C code here
     char arr[max_size];
-    printf("Enter the sentence: ");
+    char c;
+    printf("enter the sentence: ");
     fgets(arr, max_size, stdin);
-    int end = 0;
-    while (arr[end] != '\n' && arr[end] != '\0') {
-        end++;
+    printf("enter the character: ");
+    scanf("%c", &c);
+    
+    int start = 0, end = 0;
+    for(; arr[end]!='\n'; end ++){
+        
     }
-    arr[end--] = '\0';
-    while (end >= 0 && arr[end] == ' ') {
-        end--;
+    end --;
+    for(; arr[end] == ' '; end--){
+        
     }
-    int start = 0;
-    while (start <= end && arr[start] == ' ') {
-        start++;
+    
+    for(; arr[start] == ' '; start ++){
+        
     }
-
-    if (start > end) {
-        printf("No words found.\n");
-        return 0;
-    }
-
-    int max_len = 0, curr_len = 0;
-    int start_index = start, max_start = start;
-
-    for (int i = start; i <= end; i++) {
-        if (arr[i] != ' ') {
-            curr_len++;
-        } else {
-            if (curr_len > max_len) {
-                max_len = curr_len;
-                max_start = i - curr_len;
-            }
-            curr_len = 0;
+    
+    char ans [max_size];
+    int j = 0;
+    for(int i = start; i<=end; i ++){
+        if(arr[i] == c)continue;
+        else{
+            ans[j++] = arr[i];
         }
     }
-
+    ans[j] = '\0';
     
-    if (curr_len > max_len) {
-        max_len = curr_len;
-        max_start = end - curr_len + 1;
-    }
-    char ans[max_len + 1];
-    for (int i = 0; i < max_len; i++) {
-        ans[i] = arr[max_start + i];
-    }
-    ans[max_len] = '\0';
+    printf("ans is : %s", ans);
+    
 
-    printf("Longest word is: %s\n", ans);
     return 0;
 }
